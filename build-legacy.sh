023748187e021827b9c218ec6922a21f743a731e
Robert Wiley <ylee@bodhilinux.com>
2025-02-19 09:13:05 -0500

    Use Correct ec file name

--- build-legacy.sh
+++ build-legacy.sh
@@ -3,7 +3,7 @@
 
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaDetourious
-sed -i 's/MokshaDetour/Icons-Moksha-Detour/g' default.edc
+sed -i 's/MokshaDetour/Icons-Moksha-Detour/g' main.edc
 ./build.sh
-sed -i 's/Icons-Moksha-Detour/MokshaDetour/g' default.edc
+sed -i 's/Icons-Moksha-Detour/MokshaDetour/g' main.edc
 cd ..
